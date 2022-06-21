4cc24d36a560d86434a3341c39033d7135275a6c
vraghuvanshi221 <singhgabbar029@gmail.com>
2022-06-21 17:53:58 +0530

    vishal

--- src/Controller/blogController.js
+++ src/Controller/blogController.js
@@ -1,37 +1,5 @@
 const blogModel = require("../Model/blogModel")
-
-
-
-const updateBlog = async function (req, res) {
-    try {
-        let blogId = req.params.blogId
-        let blog = await blogModel.findById(blogId);
-
-        if (!blog) {
-            return res.status(404).send("No such exits blog ");
-        };
-        let blogData = req.body;
-        let updateBlog = await blogModel.findOneAndUpdate({ _id: blogId }, blogData);
-        return res.status(200).send({ status: true, data: updateBlog });
-    }
-    catch (err) {
-        return res.status(500).send({ status: false, msg: "Internal Server Error" })
-    }
-};
-
-const deleteBlog = async function (req, res) {
-    let blogId = req.query.queryParams
-    let blog = await blogModel.findById(blogId);
-    if (!blog)
-        res.status(404).send({ status: false, msg: "Data doesn't exits" })
-    // let updateBlog= await blogModel.findOneAndUpdate({_id:userId},{$set:{isDeleted:true}},{new:true})
-    // res.send ({status:true,data:updateBlog})
-};
-
-module.exports.updateBlog = updateBlog;
-module.exports.deleteBlog = deleteBlog;
 const  mongoose = require("mongoose")
-//const blogModel = require("../Model/blogModel")
 
 const getBlogs = async function (req, res) {
     try {
@@ -78,6 +46,39 @@ const getBlogs = async function (req, res) {
       res.status(500).send({ status: false, err: error.message });
     }
   };
+
+const updateBlog = async function (req, res) {
+    try {
+        let blogId = req.params.blogId
+        let blog = await blogModel.findById(blogId);
+
+        if (!blog) {
+            return res.status(404).send("No such exits blog ");
+        };
+        let blogData = req.body;
+        let updateBlog = await blogModel.findOneAndUpdate({ _id: blogId }, blogData);
+        return res.status(200).send({ status: true, data: updateBlog });
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, msg: "Internal Server Error" })
+    }
+};
+
+const deleteBlog = async function (req, res) {
+    let blogId = req.query.queryParams
+    let blog = await blogModel.findById(blogId);
+    if (!blog)
+        res.status(404).send({ status: false, msg: "Data doesn't exits" })
+    // let updateBlog= await blogModel.findOneAndUpdate({_id:userId},{$set:{isDeleted:true}},{new:true})
+    // res.send ({status:true,data:updateBlog})
+};
+module.exports.getBlogs =getBlogs
+module.exports.updateBlog = updateBlog;
+module.exports.deleteBlog = deleteBlog;
+
+//const blogModel = require("../Model/blogModel")
+
+
   
 
-  module.exports.getBlogs =getBlogs
+ 
