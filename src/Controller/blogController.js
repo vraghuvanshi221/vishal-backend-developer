4994133b671afa9fa401f4e194993fb182d29e59
Ashwin5196@ <ashwinnikose180@gmail.com>
2022-06-22 12:45:47 +0530

    ashwin

--- src/Controller/blogController.js
+++ src/Controller/blogController.js
@@ -43,7 +43,25 @@ const getBlogs = async function (req, res) {
         return res.status(500).send({ status: false, msg: "Internal Server Error" })
     }
 };
+// updated by params
+const updateBlog = async function (req, res) {
+    try {
+        let blogId = req.params.blogId
 
+        let blog = await blogModel.findById(blogId);
+        console.log(blog)
+
+        if (!blog || blog.isdeleted == true) {
+            return res.status(404).send({ status: false, msg: "no such blog exists" });
+        };
+        let blogData = req.body;
+        let updateBlog = await blogModel.findOneAndUpdate({ _id: blogId }, blogData);
+        return res.status(200).send({ status: true, data: updateBlog });
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, msg: "Internal Server Error" })
+    }
+};
 
 
 // delete By Id --> path params
