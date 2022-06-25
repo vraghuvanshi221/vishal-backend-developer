45c7b45f6b62c99ae5c589ec12f304a07bd5d950
akashguptasky <skygupta193@gmail.com>
2022-06-25 13:33:37 +0530

    Akash Gupta

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -1,7 +1,6 @@
 // ================================================ ** Import all required Modules and packages **===================================================
 
 const jwt = require('jsonwebtoken')
-const blogModel = require('../Model/blogModel')
 
 
 const isVerifyToken = function (req, res, next) {
@@ -10,47 +9,18 @@ const isVerifyToken = function (req, res, next) {
         let token = req.headers["x-api-key"]
         if (!token) return res.status(401).send({ status: false, msg: "token must be present " })
         let decodedToken = jwt.verify(token, "project1");
-        if (!decodedToken) {
-            res.status(401).send({ status: false, msg: "token is invalid" });
-        }
+        if (!decodedToken) return  res.status(401).send({ status: false, msg: "token is invalid" });
+        
+        // set author id int the request author
         req.authorId = decodedToken.authorId
         next()
     } catch (err) {
-        res.status(401).send({ status: false, msg: "token is invalid" });
+         return res.status(401).send({ status: false, msg: "token is invalid" });
     }
 };
 
-// This is a authorisezation Middleware
-const authorise = async function (req, res, next) {
-    try {
-        let token = req.headers["x-api-key"]
-
-        let decodedToken = jwt.verify(token, "project1")
-
-        let userToBeModified = req.params.blogId
-
-        let blogData = await blogModel.findById(userToBeModified)
-
-        if (!blogData) return res.status(401).send({ status: false, msg: "Invalid Blog Id" })
-
-        let userLoggedIn = decodedToken.authorId
-
-        // convert objectId to String
-        let authorId = blogData.authorId.toString()
-
-        if (authorId != userLoggedIn)
-            return res.status(403).send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
-
-
-    }
-    catch (err) {
-        res.status(500).send({ msg: "Error", error: err.message })
-    }
-
-    next()
-};
 
 
 //================================================ ** Exprots all modules here ** ===================================================
 
-module.exports = { authorise, isVerifyToken }
+module.exports = { isVerifyToken }
