5bbf481873b9ea829a70948b7b0a1e547c0f8a52
vraghuvanshi221 <singhgabbar029@gmail.com>
2022-06-29 12:36:04 +0530

    vishal

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,5 +1,9 @@
 const InternModel= require("../Models/InternModel")
 const validate=require("../Validator/validation")
+const mongoose = require("mongoose")
+const { query } = require('express');
+
+
 
 // { name: {mandatory}, email: {mandatory, valid email, unique}, mobile: {mandatory, valid mobile number, unique}, collegeId: {ObjectId, ref to college model, isDeleted: {boolean, default: false}}
 const createIntern = async function (req,res){
@@ -8,4 +12,12 @@ const createIntern = async function (req,res){
     if(!("name" in data ) || !("email" in data) || !("mobile" in data) || !("collegeId" in data)) return res.status(400).send({status:false,msg:"Don't Skip The Required Attributes ('name','email','mobile','collegeId') "})
 
 }
+
+
+const collegedetails = async function (req,res){
+    let data = req.query
+
+}
+
+module.exports.collegedetails=collegedetails
 module.exports.createIntern=createIntern
