ec93ae1962f072a82b7728557c6f5b550a35c163
kajol-shrivastava <shrivastavkajol4285@gmail.com>
2022-07-26 11:48:26 +0530

    authenticaton done

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/userModel")
+const jwt=require("jsonwebtoken")
 const bcrypt = require("bcrypt")
 const { uploadFile } = require("../AWS/aws")
 const { isValid, isValidMail, isValidMobile, isValidRequest, isValidPassword } = require("../validator/validation")
@@ -14,9 +15,9 @@ const registerUser = async function (req, res) {
     try {
         let userDetails = req.body
         let files = req.files
-        let { fname, lname, email, phone, password } = userDetails
+        let { fname, lname, email, phone, password ,address} = userDetails
 
-        let address = req.body.address
+        //let address =JSON.parse(req.body.address)
         let { shipping, billing } = address
         if (!isValidRequest(userDetails)) {
             return res.status(400).send({ status: false, msg: "Please enter details for user registration." })
@@ -59,13 +60,17 @@ const registerUser = async function (req, res) {
         if (!isValidPassword(password)) {
             return res.status(400).send({ status: false, msg: "Please enter a password which contains min 8 and maximum 15 letters,upper and lower case letters and a number" })
         }
+
+        const salt=await bcrypt.genSalt(10)
+        const hashedpass=await bcrypt.hash(password,salt)
+        password=hashedpass
         if (!address) {
             return res.status(400).send({ status: false, msg: "Please enter address for shippin and billing purpose." })
         }
         let profileImage = uploadedFileURL
         let responseBody = { fname, lname, email, profileImage, phone, password, address }
         let createUser = await userModel.create(responseBody)
-        return res.status(201).send({ status: true, data: createUser })
+        return res.status(201).send({ status: true,message:"User created successfully ",data: createUser })
     }
     catch (err) {
         res.status(500).send({ status: false, msg: err.message })
@@ -94,9 +99,13 @@ const userLogin = async function (req, res) {
             return res.status(400).send({ status: false, message: "Please enter a valid email" })
         }
 
+        if (!isValidPassword(password)) {
+            return res.status(400).send({ status: false, msg: "Please enter a password which contains min 8 and maximum 15 " })
+        }
+
         const loginUser = await userModel.findOne({ email: email })
         if (!loginUser) {
-            return res.status(401).send({ status: false, message: "Incorrect Email" }) //login email and password does not match validation.
+            return res.status(401).send({ status: false, message: "Incorrect Email" }) 
         }
 
         let hashedpass = loginUser.password
@@ -113,7 +122,7 @@ const userLogin = async function (req, res) {
         )
         res.setHeader("x-api-key", token)
         let dataToBeSend = { usedId: loginUser._id, token: token }
-        res.status(201).send({ status: true, message: 'User login successfull', data: dataToBeSend })
+        res.status(200).send({ status: true, message: 'User login successfull', data: dataToBeSend })
 
     }
     catch (err) {
@@ -127,9 +136,13 @@ const userLogin = async function (req, res) {
 
 const getUser = async function (req, res) {
     try {
-        let usedId = req.params.usedId
+        let userId = req.params.userId
         //-------------------------------------checking Authorizaton------------------------->>
-
+        if(req.loginId!=userId){
+            return res.status(403).send({ status: false, message: "User logged is not allowed to view the profile details" })
+        }
+        let userDetails=await userModel.findById(userId)
+        res.status(200).send({ status: true, message: "User profile details" ,data:userDetails})
 
     }
     catch (err) {
