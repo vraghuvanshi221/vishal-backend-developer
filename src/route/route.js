081ec4d20e6c62d2cc04f145f980a060fed391c4
debobratade-github <debobratade@gmail.com>
2022-07-27 16:29:50 +0530

    get pro-byId & del pro-byId

--- src/route/route.js
+++ src/route/route.js
@@ -2,7 +2,7 @@ const { Route53RecoveryCluster } = require('aws-sdk')
 const express= require ('express')
 const router = express.Router()
 const {registerUser, userLogin, getUser, updateUserDetails } = require('../controller/userController') 
-const{createProduct}=require("../controller/productController")
+const{createProduct, getProductsById, deleteProductById}=require("../controller/productController")
 const {authentication}=require("../middleware/auth")
 
 
