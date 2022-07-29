eb06fe6a3150385a3a5de240061c90afed39c8c1
harsh-developer <harshpratap1444@gmail.com>
2022-07-29 23:08:08 +0530

    added some validations and minor message changes

--- src/validator/validation.js
+++ src/validator/validation.js
@@ -13,6 +13,7 @@ const isValidRequest = function (data) {
   return true;
 };
 
+
 const validName = function (name) {
   return /^[a-zA-Z]{2,30}$/.test(name)
 }
@@ -22,36 +23,43 @@ const isValidMail = function (v) {
   return /^([0-9a-z]([-_\\.]*[0-9a-z]+)*)@([a-z]([-_\\.]*[a-z]+)*)[\\.]([a-z]{2,9})+$/.test(v);
 };
 
+
 const isValidMobile = function (num) {
   return /^[6789]\d{9}$/.test(num);
 };
+
 
 const isValidStreet = function (street) {
   return /^([a-zA-Z0-9 ]{2,50})*$/.test(street)
 }
 
+
 const isValidCity = function (city) {
   return /^[a-zA-z]+([\s][a-zA-Z]+)*$/.test(city)
 }
 
+
 const isValidPin = function (pincode) {
   return /^\d{6}$/.test(pincode)
 }
+
 
 const removeExtraSpace = function (value) {
   const res = value.split(" ").filter(word => word).join(" ")
   return res
 }
 
+
 const isValidObjectId = function (objectId) {
   return mongoose.Types.ObjectId.isValid(objectId)
 }
+
 
 const isValidSize = function (size) {
 
   let flag = 0
   for (x in size) {
-    let ret = ["S", "XS", "M", "X", "L", "XXL", "XL"].indexOf(size[x]) != -1
+    let ret = ["XS", "S", "M", "L", "XL", "XXL"].indexOf(size[x]) != -1
     if (!ret) {
       flag = 1
       break
@@ -70,15 +78,18 @@ const isValidPassword = function (password) {
 }
 
 
-
-const isValidNumberInt=function (value){
-  return (!(isNaN(Number(value)))&&(value%1===0))
+const isValidNumberInt = function (value) {
+  return (!(isNaN(Number(value))) && (value % 1 === 0))
 }
-const isValidNumber=function(value){
+
+
+const isValidNumber = function (value) {
   return (!(isNaN(Number(value))))
 }
+
+
 module.exports = {
   isValid, validName, isValidMail, isValidRequest,
   isValidMobile, isValidPassword, isValidStreet, isValidCity, isValidPin, isValidObjectId, removeExtraSpace,
-  isValidSize, isValidSize,isValidNumberInt,isValidNumber
+  isValidSize, isValidNumberInt, isValidNumber
 }
