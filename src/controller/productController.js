3a514b21da26dfe0b3ab254172a2eb6e8cb5e4a9
vraghuvanshi221 <singhgabbar029@gmail.com>
2022-07-30 13:56:27 +0530

    merging

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -221,7 +221,7 @@ const updateProductDetails = async function (req, res) {
         if ((Object.keys(data).length == 0) && (!isValid(files))) {
             return res.status(400).send({ status: 400, msg: "Invalid request" });
         }
-        let { title, description, price, currencyId, currencyFormat, isFreeShipping, productImage, style, availableSizes, installments } = data;
+        let { title, description, price, currencyId, currencyFormat, isFreeShipping, style, availableSizes, installments } = data;
         let obj = {}
 
         if (title) {
