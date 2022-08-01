34e13c4d181501a2d3efb1f4749c74ad2dce3078
vraghuvanshi221 <singhgabbar029@gmail.com>
2022-08-01 14:56:14 +0530

    update

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose')
 
 const cartSchema = new mongoose.Schema({
  
-
+    userId: {type:ObjectId, refs:"User",required:true, unique:true},
+    items: [{
+      productId: {type:ObjectId,refs:"Product", required:true},
+      quantity: {type:Number, require:true, min:1}
+    }],
+    totalPrice: {type:Number, required:true, comment: "Holds total price of all the items in the cart"},
+    totalItems: {type:Number, required:true, comment: "Holds total number of items in the cart"},
 
 
 
